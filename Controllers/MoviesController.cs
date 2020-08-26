ea4fdf15e3205c9b2846a8542dc0c441bb213a2f
ramez <herrickramez@gmail.com>
2020-08-26 17:15:34 -0400

    submission

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab23_MoviesEntityDataBaseFirst.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Lab23_MoviesEntityDataBaseFirst.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly MoviesDbContext _context;
+        public MoviesController (MoviesDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var movielist = _context.Movie.ToList();
+            return View(movielist);
+        }
+
+
+        [HttpGet]
+        public IActionResult RegisterMovie()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RegisterMovie(Movie movie)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Movie.Add(movie);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteMovie (int id)
+        {
+            var foundmovie = _context.Movie.Find(id);
+            if(foundmovie != null)
+            {
+                _context.Movie.Remove(foundmovie);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/MoviesDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Lab23_MoviesEntityDataBaseFirst.Models
+{
+    public partial class MoviesDbContext : DbContext
+    {
+        public MoviesDbContext()
+        {
+        }
+
+        public MoviesDbContext(DbContextOptions<MoviesDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Movie> Movie { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=.\\sqlexpress;Database=MoviesDb;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>(entity =>
+            {
+                entity.Property(e => e.Genre).HasMaxLength(20);
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
